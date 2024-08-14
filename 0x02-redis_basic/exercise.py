8d3880c02ab926e5635529713c78e096ec99d8ba
Trevor <Trevormahinda@gmail.com>
2024-08-14 20:18:15 +0300

    store method for cache

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""creates a class Cache"""
+
+import redis
+import uuid
+
+
+class Cache():
+
+    """defines a class Cache"""
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: str) -> str:
+        """store method that takes data and stores in redis"""
+        random_key = str(uuid.uuid4())
+        self._redis.mset({random_key: data})
+        return random_key
