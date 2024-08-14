848f4361d5aa8066db17a4249fd460d8febf97ef
Trevor <Trevormahinda@gmail.com>
2024-08-14 20:36:50 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -19,3 +19,25 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.mset({random_key: data})
         return random_key
+    
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+    
+    def get_str(self, key: str) -> str:
+        """automatically parameters with correct 
+        converson function"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+    
+    def get_int(self, key: str) -> int:
+        """conversion function"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
