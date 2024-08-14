bea66cbf4cb91858866d1015edb84709041488e2
Trevor <Trevormahinda@gmail.com>
2024-08-14 20:26:11 +0300

    store method for cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,5 +17,5 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method that takes data and stores in redis"""
         random_key = str(uuid.uuid4())
-        self._redis.set({random_key: data})
+        self._redis.mset({random_key: data})
         return random_key
