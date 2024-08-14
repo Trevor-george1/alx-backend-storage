de655e3defd1357089bfa0b64382ad91c9fdc5e4
Trevor <Trevormahinda@gmail.com>
2024-08-14 20:20:49 +0300

    store method for cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb
 
-    def store(self, data: str) -> str:
+    def store(self, data: any) -> str:
         """store method that takes data and stores in redis"""
         random_key = str(uuid.uuid4())
         self._redis.mset({random_key: data})
